37dd9a3d733cc49d2ccd7d0adaa3240895054122
tony-ing <tony-ing@live.com>
2019-07-15 14:10:38 +0800

    update for loop

--- test.c
+++ test.c
@@ -2,12 +2,14 @@
 #include<stdlib.h>
 
 int main(char *argv[], int argc){
+	int i;	
 	if(argc<1){
 		printf("need more arguments\n");
 		return -1;
 	}
-
-	printf("helloworld\n");
+	for(i =0; i<5;i++){
+		printf("helloworld\n");
+	}
 	return 0;
 }
 
